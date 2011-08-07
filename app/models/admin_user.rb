a5b67eed9828a0498c340b123b70567652a10f97
Michael Koper <michaelkoper@gmail.com>
2011-08-07 18:38:21 +0200

    deleting all the typus data

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,8 +1,8 @@
 class AdminUser < ActiveRecord::Base
-
-  ROLE = Typus::Configuration.roles.keys.sort
-  LOCALE = Typus.locales
-
-  enable_as_typus_user
+  # 
+  # ROLE = Typus::Configuration.roles.keys.sort
+  # LOCALE = Typus.locales
+  # 
+  # enable_as_typus_user
 
 end
