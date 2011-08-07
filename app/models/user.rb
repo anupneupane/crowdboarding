a7d4b5c349ef6aaed90ef0faf508f52b5dc75b74
Michael Koper <michaelkoper@gmail.com>
2011-08-07 19:50:04 +0200

    geocode by ipaddress doesnt work... you cant define a location by ip address precisely

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,11 +23,7 @@ class User < ActiveRecord::Base
   has_many :authentications
   has_many :notifications
   belongs_to :default_city, :class_name => 'City'
-  
-  geocoded_by :last_sign_in_ip,
-    :latitude => :current_location_latitude, :longitude => :current_location_longitude
-  after_validation :geocode, :if => :last_sign_in_ip_changed?
-  
+    
   reverse_geocoded_by :current_location_latitude, :current_location_longitude,
     :address => :current_location_address
   after_validation :reverse_geocode, :if => :current_location_longitude_changed?
