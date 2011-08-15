04391cdc7247983ad31ae0d95c4173e981ec74f8
Michael Koper <michaelkoper@gmail.com>
2011-08-15 02:12:11 +0200

    added method to sanitize sql to ActiveRecord because sanitize_sql_array is a private method and cant be accessed.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,8 +6,9 @@ class EventsController < ApplicationController
   # GET /events.xml
   def index
       conditions = []
-      conditions << "country_id = #{params[:country_id]}" if params[:country_id].present?
-      conditions << "LOWER(city_name) LIKE LOWER('%#{params[:city_name]}%')" if params[:city_name].present?
+      conditions << Event.escape_sql(["country_id = ?", params[:country_id]]) if params[:country_id].present?
+      conditions << Event.escape_sql(["LOWER(city_name) LIKE LOWER(?)", "%#{params[:city_name].downcase}%"]) if params[:city_name].present?
+      
       @events = Event.where(conditions.join(" AND ")).page(params[:page]).per(10)
       # @events = Event.joins(:taggings, :tags).where(["events.name LIKE ? OR events.city_name LIKE ? ", search_string, search_string]).page(params[:page]).per(10)
       # @events = Event.recent.page(params[:page]).per(10)
