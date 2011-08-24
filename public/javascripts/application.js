7316afc1d69951f61c1c89ed3758d3a05e281ecd
Michael Koper <michaelkoper@gmail.com>
2011-08-24 22:31:37 +0200

    made first version notification window

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 	mouseOverHelmet();
 	clickHelmet();
 	showNotifyBar();
+	hoverNotifications();
+	clickNotifications();
 });
 
 mouseOverHelmet = function(){
@@ -22,5 +24,29 @@ showNotifyBar = function(){
 clickHelmet = function(){
 	$('div#helmet').click(function(){
 		window.location = "/helmet"
+	});
+}
+
+hoverNotifications = function(){
+	$('li#notifications a, li.highlighted_plus a').hover(function(){
+		$(this).toggleClass("hover");
+	});
+}
+
+clickNotifications = function(){
+	$('li#notifications a').click(function(){
+		$('div#notifications_window').show();
+		var opts = {
+		  lines: 12, // The number of lines to draw
+		  length: 6, // The length of each line
+		  width: 5, // The line thickness
+		  radius: 10, // The radius of the inner circle
+		  color: '#B10109', // #rbg or #rrggbb
+		  speed: 1, // Rounds per second
+		  trail: 100, // Afterglow percentage
+		  shadow: false // Whether to render a shadow
+		};
+		var target = document.getElementById('notifications_body');
+		var spinner = new Spinner(opts).spin(target);
 	});
 }
