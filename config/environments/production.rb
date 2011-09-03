c54f60f7be51b9f807e1636a8017e97bfafbe695
Michael Koper <michaelkoper@gmail.com>
2011-09-04 01:52:27 +0200

    version bumb to Rails 3.1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,16 +1,27 @@
 Crowdboarding::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = false
+  
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
+  
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
+  
+  # Generate digests for assets URLs
+  config.assets.digest = true
 
   # Specifies the header that your server uses for sending files
-  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
   # For nginx:
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
@@ -26,10 +37,6 @@ Crowdboarding::Application.configure do
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
-
-  # Disable Rails's static asset server
-  # In production, Apache or nginx will already do this
-  config.serve_static_assets = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
