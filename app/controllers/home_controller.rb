cdec87faf12648b7bfbe3e8a39cca5b499295371
Michael Koper <michaelkoper@gmail.com>
2011-09-04 17:12:36 +0200

    temp delete tags in home because of heroku error

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < ApplicationController
     @events = Event.from_now.recent.page(params[:page]).per(10)
     @events_from_now = Event.from_now.limit(5)
     @active_users = User.limit(10)
-    @tags = Event.tag_counts_on(:tags)
+    # @tags = Event.xtag_counts_on(:tags)
   end
 end
