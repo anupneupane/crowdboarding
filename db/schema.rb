9b1b583fa9c4c80de13262d7e7da1ec647b09df0
Michael Koper <michaelkoper@gmail.com>
2011-09-04 19:04:16 +0200

    ie8 isnt in the pipeline

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,148 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20110605163509) do
+
+  create_table "attendances", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "attendances", ["event_id"], :name => "index_attendances_on_event_id"
+  add_index "attendances", ["user_id"], :name => "index_attendances_on_user_id"
+
+  create_table "authentications", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "provider",   :limit => 20
+    t.string   "uid"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "authentications", ["user_id"], :name => "index_authentications_on_user_id"
+
+  create_table "cities", :force => true do |t|
+    t.string  "name",       :limit => 100, :null => false
+    t.integer "country_id",                :null => false
+  end
+
+  create_table "comments", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "ancestry"
+    t.text     "body"
+    t.integer  "commentable_id"
+    t.string   "commentable_type", :limit => 14
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["ancestry"], :name => "index_comments_on_ancestry"
+  add_index "comments", ["commentable_id", "commentable_type"], :name => "index_comments_on_commentable"
+  add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
+
+  create_table "countries", :force => true do |t|
+    t.string "name",         :null => false
+    t.string "country_code", :null => false
+  end
+
+  add_index "countries", ["country_code"], :name => "index_countries_on_country_code", :unique => true
+  add_index "countries", ["name"], :name => "index_countries_on_name", :unique => true
+
+  create_table "events", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "name",            :limit => 100
+    t.datetime "starts_at"
+    t.text     "description"
+    t.string   "street",          :limit => 100
+    t.integer  "country_id"
+    t.string   "contact_details", :limit => 100
+    t.float    "lat"
+    t.float    "lng"
+    t.integer  "city_id"
+    t.integer  "users_count",                    :default => 0
+    t.integer  "comments_count",                 :default => 0
+    t.integer  "views_count",                    :default => 0
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "events", ["city_id"], :name => "index_events_on_city_id"
+  add_index "events", ["country_id"], :name => "index_events_on_country_id"
+  add_index "events", ["starts_at"], :name => "index_events_on_starts_at"
+
+  create_table "friendships", :force => true do |t|
+    t.integer  "user_id",                  :null => false
+    t.integer  "friend_id",                :null => false
+    t.string   "type",       :limit => 10
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "friendships", ["friend_id"], :name => "index_friendships_on_friend_id"
+  add_index "friendships", ["user_id"], :name => "index_friendships_on_user_id"
+
+  create_table "notifications", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "body",       :limit => 100
+    t.boolean  "read",                      :default => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "notifications", ["user_id"], :name => "index_notifications_on_user_id"
+
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type", :limit => 20
+    t.integer  "tagger_id"
+    t.string   "tagger_type",   :limit => 20
+    t.string   "context",       :limit => 20
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
+
+  create_table "tags", :force => true do |t|
+    t.string "name"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                                     :default => "",   :null => false
+    t.string   "encrypted_password",         :limit => 128, :default => "",   :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                             :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "name",                       :limit => 100
+    t.string   "nickname",                   :limit => 40
+    t.text     "bio"
+    t.text     "quiver"
+    t.date     "birthdate"
+    t.integer  "default_city_id"
+    t.integer  "country_id"
+    t.integer  "profile_views",                             :default => 0
+    t.boolean  "event_friend_updates",                      :default => true
+    t.float    "current_location_latitude"
+    t.float    "current_location_longitude"
+    t.string   "current_location_address"
+    t.string   "users"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
