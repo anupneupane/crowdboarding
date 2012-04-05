a02b0c9210408c4fa6a595cb7185dcdeef2cdc20
Michael Koper <michaelkoper@gmail.com>
2012-04-05 18:19:45 +0200

    ..... :(

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,5 +1,4 @@
 Tire.configure do
   url "http://index.bonsai.io"
 end
-
-BONSAI_INDEX_NAME = URL(ENV['BONSAI_INDEX_URL']).path[1..-1]
+BONSAI_INDEX_NAME = URI.parse(ENV['BONSAI_INDEX_URL']).path[1..-1]
